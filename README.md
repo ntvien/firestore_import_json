c536fca9c1bf9e89d18a2a07d45fcaac4eaa8342
Fiction_NTV <thevien898@gmail.com>
2021-07-04 00:21:32 +0700

    firestore import data json
    firestore import data json

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# firestore_import_json
-firestore_import_json
+# Simple Firestore Import
+
+Easily import data into Firebase Cloud Firestore from json and yaml files.
+
+## Installation
+
+Clone the repo:
+
+    $ git clone 
+
+Move into the directory:
+
+    $ cd import_data
+    
+Install dependencies:
+
+    $ pip install -r requirements.txt
+
+## Usage
+
+Provide your **Firebase or Google Cloud service account key**, data file and the name of the collection.
+
+    $ python import_data_firestore.py [path/to/service_account.json] [path/to/dataFile(.json|.yaml|.yml)] [name of collection]
+    
+Or without arguments:
+
+    $ python import_data_firestore.py
+    Path to serviceAccountKey.json: [path/to/service_account.json]
+    Path to data file: [path/to/dataFile(.json|.yaml|.yml)]
+    Name of collection: [name of collection]
+
+Ex: 
+    $ python import_data_firestore.py service_account.json data.json user
+    
+#### Note
+
+The program expects an array of objects (dictionaries) to work.
+
+**CORRECT DATA FILE FORMAT**
+
+```json
+[
+    {
+        "name": "Vien Nguyen",
+        "email": "thevien898@gmail.com"
+    },
+    {
+        "name": "Fiction",
+        "email": "thevien@gmail.com"
+    }
+]
+```
+
+```yaml
+
+---
+- name: Vien Nguyen
+  email: thevien898@gmail.com
+- name: Fiction
+  email: thevien@gmail.com
+```
---
+++ import_data_simple/import_data_firestore.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import json
+import yaml
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+
+def import_data(service_account_key_path, data_file, collection_name):
+    try:
+        cred = credentials.Certificate(service_account_key_path)
+        firebase_admin.initialize_app(cred)
+
+        db = firestore.client()
+
+        data = get_data(data_file)
+        check_data(data)
+
+        doc_ref = db.collection(collection_name)
+        for datum in data:
+            doc_ref.add(datum)
+            print("Added: {}".format(datum))
+
+    except Exception as error:
+        print("\nERROR: {}".format(str(error)))
+    else:
+        print("\nImport complete")
+
+
+def check_data(data):
+    if isinstance(data, (list, tuple,)):
+        for datum in data:
+            if not isinstance(datum, (dict,)):
+                raise ValueError("An object expected, got {}".format(type(datum)))
+    else:
+        raise ValueError("An array expected, got {}".format(type(data)))
+
+
+def get_data(data_file):
+    allowed_file_extensions = [".json", ".yaml", ".yml"]
+    filename, file_extension = os.path.splitext(data_file)
+    file_extension = file_extension.lower()
+
+    if file_extension in allowed_file_extensions:
+        if file_extension == ".json":
+            return get_json_data(data_file)
+        elif file_extension == ".yaml" or file_extension == ".yml":
+            return get_yaml_data(data_file)
+
+    raise ValueError("Invalid file type. Allowed file types include {}".format(allowed_file_extensions))
+
+
+def get_json_data(data_file):
+    with open(data_file, "r") as read_file:
+        return json.load(read_file)
+
+
+def get_yaml_data(data_file):
+    with open(data_file, "r") as read_file:
+        return yaml.load(read_file, Loader=yaml.FullLoader)
+
+
+if __name__ == "__main__":
+    try:
+        if len(sys.argv) == 4:
+            service_account_path = sys.argv[1]
+            data_file_path = sys.argv[2]
+            name_of_collection = sys.argv[3]
+        else:
+            service_account_path = input("Path to serviceAccountKey.json: ")
+            data_file_path = input("Path to data file: ")
+            name_of_collection = input("Name of collection: ")
+
+        import_data(service_account_path, data_file_path, name_of_collection)
+    except KeyboardInterrupt as keyboard_error:
+        print("\nProcess interrupted")
+    finally:
+        print("\nGood Bye!")
